86b981355f304d2e26605429bc91a9aa56e1ddbd
mzakharow <098098789>
2019-04-19 00:40:11 +0300

    31

--- shop/forms.py
+++ shop/forms.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 
 class OrderForm(forms.Form):
 
-    name = forms.CharField()
-    last_name = forms.CharField(required=False)
+    name = forms.CharField(label='Имя')
+    last_name = forms.CharField(required=False, label='Фамилия')
     phone = forms.CharField()
     buying_type = forms.ChoiceField(widget=forms.Select(), choices=([("self", "Самовывоз"), ("delivery", "Доставка")]))
     date = forms.DateField(widget=forms.SelectDateWidget(), initial=timezone.now())
@@ -15,13 +15,13 @@ class OrderForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(OrderForm, self).__init__(*args, **kwargs)
-        self.fields['name'].label = 'Имя'
-        self.fields['last_name'].label = 'Фамилия'
-        self.fields['phone'].label = 'Телефон'
+        # self.fields['name'].label = 'Имя'
+        # # self.fields['last_name'].label = 'Фамилия'
+        # self.fields['phone'].label = 'Телефон'
         self.fields['phone'].help_text = 'Номер телефона в формате +79011234567'
-        self.fields['buying_type'].label = 'Способ получения'
-        self.fields['address'].label = 'Адрес доставки'
-        self.fields['address'].help_text = '*Обязательно указывайте город'
-        self.fields['comments'].label = 'Комментарий'
+        # self.fields['buying_type'].label = 'Способ получения'
+        # self.fields['address'].label = 'Адрес доставки'
+        # self.fields['address'].help_text = '*Обязательно указывайте город'
+        # self.fields['comments'].label = 'Комментарий'
         self.fields['date'].label = 'Дата доставки'
         self.fields['date'].help_text = 'Доставка осуществляется на следующий день после оформления заказа'
